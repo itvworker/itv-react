8e9725e5191123d3526705fba95ca7b2bd5d7ee4
xiekx2 <xiekx2@midea.com>
2020-12-08 20:37:44 +0800

    环境搭建

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter as Router, Link, Route } from 'react-router-dom';
+
 import './index.css';
-import App from './App';
+import App from './pages/App';
 import reportWebVitals from './reportWebVitals';
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Router>
+        <Route path="/" component={App}/>
+    </Router>,
   document.getElementById('root')
 );
 
---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+
+
+let content= 
+    
+
+export default <Router>
+<Route path="/" component={App}/>
+</Router>
